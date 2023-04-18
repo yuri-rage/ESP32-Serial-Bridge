1cc69386752cd8c62869afb48d021422bb0cb745
Yuri <yuri.rage@gmail.com>
2023-04-18 15:30:47 -0500

    Added UDP + bug fixes

--- ESP-Serial-Bridge.ino
+++ ESP-Serial-Bridge.ino
@@ -1,25 +1,31 @@
 /*********************************************************************************
  * ESP-Serial-Bridge
- * 
+ *
  * Simple WiFi Serial Bridge for Espressif microcontrollers
- * 
+ *
  * Forked from https://github.com/AlphaLima/ESP32-Serial-Bridge
- * 
- * Added compatibility for ESP8266, WiFi reconnect on failure, and mDNS discovery.
- * 
+ *
+ * Added compatibility for ESP8266, WiFi reconnect on failure, and mDNS
+ *discovery.
+ *
  * Note: ESP8266 is limited to 115200 baud and may be somewhat unreliable in
  *       this application.
- * 
+ *
  *   -- Yuri - Aug 2021
- * 
+ *
+ * Fixed compatibility with Arduino framework 2.0 -- Yuri - Apr 2023
+ * Implemented UDP + bug fixes                    -- Yuri - Apr 2023
+ *
  * Disclaimer: Don't use for life support systems or any other situation
  * where system failure may affect user or environmental safety.
-*********************************************************************************/
+ *********************************************************************************/
+
+#include <Arduino.h>
 
 #ifdef ESP32
-#include <esp_wifi.h>
-#include <WiFi.h>
 #include <ESPmDNS.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
@@ -28,286 +34,309 @@
 
 #include "config.h"
 
-#ifdef OTA_HANDLER  
-#include <ArduinoOTA.h> 
+#ifdef OTA_HANDLER
+#include <ArduinoOTA.h>
 #endif
 
 #ifdef ESP32
+
+#ifdef PROTOCOL_UDP
+#include <AsyncUDP.h>
+AsyncUDP udp;
+uint16_t udp_port[NUM_COM] = {SERIAL0_UDP_PORT, SERIAL1_UDP_PORT,
+                              SERIAL2_UDP_PORT};
+#endif
+
 #ifdef BLUETOOTH  // inside the ESP32 condition, since ESP8266 doesn't have BT
 #include <BluetoothSerial.h>
-BluetoothSerial SerialBT; 
-#endif
+BluetoothSerial SerialBT;
 #endif
 
-#ifdef ESP32
 HardwareSerial Serial_one(1);
 HardwareSerial Serial_two(2);
-HardwareSerial* COM[NUM_COM] = {&Serial, &Serial_one , &Serial_two};
+HardwareSerial* COM[NUM_COM] = {&Serial, &Serial_one, &Serial_two};
 #elif defined(ESP8266)
-SoftwareSerial Serial_zero;
-SoftwareSerial Serial_one;
+SoftwareSerial Serial_zero(SERIAL0_RXPIN, SERIAL0_TXPIN);
+SoftwareSerial Serial_one(SERIAL1_RXPIN, SERIAL1_TXPIN);
 SoftwareSerial* COM[NUM_COM] = {&Serial_zero, &Serial_one};
 #endif
 
-#define MAX_NMEA_CLIENTS 4
 #ifdef PROTOCOL_TCP
 #include <WiFiClient.h>
 WiFiServer server_0(SERIAL0_TCP_PORT);
 WiFiServer server_1(SERIAL1_TCP_PORT);
+
 #ifdef ESP32
 WiFiServer server_2(SERIAL2_TCP_PORT);
-WiFiServer *server[NUM_COM]={&server_0, &server_1, &server_2};
+WiFiServer* server[NUM_COM] = {&server_0, &server_1, &server_2};
 #elif defined(ESP8266)
-WiFiServer *server[NUM_COM] = {&server_0, &server_1};
+WiFiServer* server[NUM_COM] = {&server_0, &server_1};
 #endif
 WiFiClient TCPClient[NUM_COM][MAX_NMEA_CLIENTS];
 #endif
 
-uint8_t buf1[NUM_COM][bufferSize];
-uint8_t buf2[NUM_COM][bufferSize];
+uint8_t buf1[NUM_COM][BUFFERSIZE];
+uint8_t buf2[NUM_COM][BUFFERSIZE];
 
 #ifdef ESP32
-uint16_t i1[NUM_COM]={0, 0, 0};
-uint16_t i2[NUM_COM]={0, 0, 0};
+uint16_t i1[NUM_COM] = {0, 0, 0};
+uint16_t i2[NUM_COM] = {0, 0, 0};
 #elif defined(ESP8266)
-uint16_t i1[NUM_COM]={0, 0};
-uint16_t i2[NUM_COM]={0, 0};
+uint16_t i1[NUM_COM] = {0, 0};
+uint16_t i2[NUM_COM] = {0, 0};
 #endif
 
-uint8_t BTbuf[bufferSize];
+uint8_t BTbuf[BUFFERSIZE];
 uint16_t iBT = 0;
 
 #ifdef MODE_STA
+#if defined(ESP32)
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
+#endif
+#if defined(ESP8266)
+    WiFiEventHandler stationDisconnectedHandler;
+    void WiFiStationDisconnected(
+        const WiFiEventSoftAPModeStationDisconnected& evt) {
+#endif
+        debug.print("WiFi disconnected: ");
 #ifdef ESP32
-void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+        debug.println(info.wifi_sta_disconnected.reason);
+#endif
+        debug.println("Trying to reconnect..");
+        WiFi.begin(SSID, PASSWD);
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            debug.print(".");
+        }
+        debug.println("connected");
+        debug.print("IP address: ");
+        debug.println(WiFi.localIP());
+    }
+#endif
+
+    void setup() {
+        delay(500);
+
+#ifdef ESP32
+        COM[0]->begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
+        COM[1]->begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
+        COM[2]->begin(UART_BAUD2, SERIAL_PARAM2, SERIAL2_RXPIN, SERIAL2_TXPIN);
 #elif defined(ESP8266)
-WiFiEventHandler stationDisconnectedHandler;
-void WiFiStationDisconnected(const WiFiEventSoftAPModeStationDisconnected& evt) {
+    COM[0]->begin(UART_BAUD0);
+    COM[1]->begin(UART_BAUD1);
+    debug.begin(UART_BAUD0);
 #endif
-  if (debug) Serial.print("WiFi disconnected: ");
+
+        debug.print("\n\nWiFi serial bridge ");
+        debug.println(VERSION);
+
+#ifdef MODE_AP
+        debug.println("Open ESP Access Point Mode");
+        WiFi.mode(WIFI_AP);
+        WiFi.softAP(SSID, PASSWD);  // configure SSID and password for softAP
+        delay(2000);            // VERY IMPORTANT
+        WiFi.softAPConfig(STATIC_IP, STATIC_IP,
+                          NETMASK);  // configure ip address for softAP
+#endif
+
+#ifdef MODE_STA
+        debug.println("Open ESP Station Mode");
+        WiFi.mode(WIFI_STA);
 #ifdef ESP32
-  if (debug) Serial.println(info.disconnected.reason);
-#endif
-  if (debug) Serial.println("Trying to reconnect..");
-  WiFi.begin(ssid, pw);
-  while (WiFi.status() != WL_CONNECTED) {   
-    delay(500);
-    if (debug) Serial.print(".");
-  }
-  if (debug) Serial.println("connected");
-  if (debug) Serial.print("IP address: ");
-  if (debug) Serial.println(WiFi.localIP());
-}
-#endif
-
-void setup() {
-
-  delay(500);
-
-#ifdef ESP32
-  COM[0]->begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
-  COM[1]->begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
-  COM[2]->begin(UART_BAUD2, SERIAL_PARAM2, SERIAL2_RXPIN, SERIAL2_TXPIN);
+        WiFi.onEvent(WiFiStationDisconnected,
+                     ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
 #elif defined(ESP8266)
-  COM[0]->begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
-  COM[1]->begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
-  if (debug) Serial.begin(UART_BAUD0);
+    stationDisconnectedHandler =
+        WiFi.onSoftAPModeStationDisconnected(&WiFiStationDisconnected);
 #endif
-  
-  if (debug) Serial.print("\n\nWiFi serial bridge ");
-  if (debug) Serial.println(VERSION);
-  
-  #ifdef MODE_AP 
-  if (debug) Serial.println("Open ESP Access Point mode");
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid, pw); // configure ssid and password for softAP
-  delay(2000); // VERY IMPORTANT
-  WiFi.softAPConfig(ip, ip, netmask); // configure ip address for softAP
-  #endif
+        WiFi.begin(SSID, PASSWD);
+        debug.print("Connecting to: ");
+        debug.print(SSID);
+        debug.print("..");
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            debug.print(".");
+        }
+        debug.println("connected");
+        debug.print("IP address: ");
+        debug.println(WiFi.localIP());
 
-  #ifdef MODE_STA
-  if (debug) Serial.println("Open ESP Station Mode");
-  WiFi.mode(WIFI_STA);
-  #ifdef ESP32
-  WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
-  #elif defined(ESP8266)
-  stationDisconnectedHandler = WiFi.onSoftAPModeStationDisconnected(&WiFiStationDisconnected);
-  #endif
-  WiFi.begin(ssid, pw);
-  if (debug) Serial.print("Connecting to: ");
-  if (debug) Serial.print(ssid);
-  if (debug) Serial.print("..");
-  while (WiFi.status() != WL_CONNECTED) {   
-    delay(500);
-    if (debug) Serial.print(".");
-  }
-  if (debug) Serial.println("connected");
-  if (debug) Serial.print("IP address: ");
-  if (debug) Serial.println(WiFi.localIP());
-
-  if(!MDNS.begin(mdns_name)) {
-     if (debug) Serial.println("Error starting mDNS");
-  } else {
-    if (debug) Serial.print("Started mDNS, discoverable as: ");
-    if (debug) Serial.println(mdns_name);
-    MDNS.addService("telnet", "tcp", SERIAL0_TCP_PORT);
-    MDNS.addService("telnet", "tcp", SERIAL1_TCP_PORT);
-    #ifdef ESP32
-    MDNS.addService("telnet", "tcp", SERIAL2_TCP_PORT);
-    #endif
-  }
-  #endif
-
-#ifdef ESP32
-#ifdef BLUETOOTH
-  if (debug) Serial.println("Open Bluetooth Server");  
-  SerialBT.begin(ssid); //Bluetooth device name
-#endif
+        if (!MDNS.begin(HOSTNAME)) {
+            debug.println("Error starting mDNS");
+        } else {
+            debug.print("Started mDNS, discoverable as: ");
+            debug.println(HOSTNAME);
+            MDNS.addService("_telnet", "_tcp", SERIAL0_TCP_PORT);
+        }
 #endif
 
-#ifdef OTA_HANDLER  
-    ArduinoOTA.onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
+#ifdef OTA_HANDLER
+        ArduinoOTA.onStart([]() {
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH)
+                type = "sketch";
+            else  // U_SPIFFS
+                type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    });
-    ArduinoOTA.onEnd([]() {
-      Serial.println("\nEnd");
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-  // if DNSServer is started with "*" for domain name, it will reply with
-  // provided IP to all DNS request
+            // NOTE: if updating SPIFFS this would be the place to unmount
+            // SPIFFS using SPIFFS.end()
+            Serial.println("Start updating " + type);
+        });
+        ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
+        ArduinoOTA.onError([](ota_error_t error) {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR)
+                Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR)
+                Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR)
+                Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR)
+                Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR)
+                Serial.println("End Failed");
+        });
+        // if DNSServer is started with "*" for domain name, it will reply with
+        // provided IP to all DNS request
 
-  ArduinoOTA.begin();
-#endif   
+        ArduinoOTA.begin();
+#endif
 
 #ifdef PROTOCOL_TCP
-  for(int num=0; num < NUM_COM ; num++) {
-    if (debug) {
-      Serial.print("Starting TCP Server ");
-      Serial.println(num + 1);
-    }
-    server[num]->begin(); // start TCP server 
-    server[num]->setNoDelay(true);
-  }
+        for (int num = 0; num < NUM_COM; num++) {
+            debug.print("Starting TCP Server ");
+            debug.println(num + 1);
+            server[num]->begin();  // start TCP server
+            server[num]->setNoDelay(true);
+        }
+#endif
+
+#ifdef ESP32
+#ifdef PROTOCOL_UDP
+        bool listening = false;
+        for (uint16_t num = 0; num < NUM_COM; num++) {
+            if (udp.listen(udp_port[num])) {
+                listening = true;  // at least one port is opened
+                debug.printf("Listening on UDP port %d\n", udp_port[num]);
+            }
+        }
+        if (listening) {
+            udp.onPacket([](AsyncUDPPacket packet) {
+                for (uint16_t num = 0; num < NUM_COM; num++) {
+                    if (packet.remotePort() == udp_port[num]) {
+                        COM[num]->write(packet.data(), packet.length());
+                        break;
+                    }
+                }
+            });
+        }
+#endif
+#ifdef BLUETOOTH
+        debug.printf("Bluetooth discoverable at %s\n", SSID);
+        SerialBT.begin(SSID);  // Bluetooth device name
+#endif
 #endif
 
 #ifdef BATTERY_SAVER
 #ifdef ESP32
-  esp_err_t esp_wifi_set_max_tx_power(50);  //lower WiFi Power
+        esp_err_t esp_wifi_set_max_tx_power(50);  // lower WiFi Power
 #elif defined(ESP8266)
-  WiFi.setOutputPower(15);
+    WiFi.setOutputPower(15);
 #endif
 #endif
+    }
 
-}
-
-void loop() 
-{  
-#ifdef OTA_HANDLER  
-  ArduinoOTA.handle();
+    void loop() {
+#ifdef OTA_HANDLER
+        ArduinoOTA.handle();
 #endif
 
 #ifdef ESP32
 #ifdef BLUETOOTH
-  if(SerialBT.hasClient()) 
-  {
-    while(SerialBT.available())
-    {
-      BTbuf[iBT] = SerialBT.read();
-      if(iBT <bufferSize-1) iBT++;
-    }
-    // this is unchanged from the AlphaLima implementation
-    // all serial communication (up to 3 ports) is echoed on BlueTooth
-    // may not be ideal...
-    for(int num= 0; num < NUM_COM ; num++)
-      COM[num]->write(BTbuf, iBT);
-    iBT = 0;
-  }  
+        if (SerialBT.hasClient()) {
+            while (SerialBT.available()) {
+                BTbuf[iBT] = SerialBT.read();
+                iBT++;
+                if (iBT == BUFFERSIZE - 1) break;
+            }
+            COM[BLUETOOTH]->write(BTbuf, iBT);
+            iBT = 0;
+        }
 #endif
 #endif
 
 #ifdef PROTOCOL_TCP
-  for(int num=0; num < NUM_COM ; num++)
-  {
-    if (server[num]->hasClient())
-    {
-      for(byte i = 0; i < MAX_NMEA_CLIENTS; i++){
-        //find free/disconnected spot
-        if (!TCPClient[num][i] || !TCPClient[num][i].connected()){
-          if(TCPClient[num][i]) TCPClient[num][i].stop();
-          TCPClient[num][i] = server[num]->available();
-          if (debug) Serial.print("New client for COM"); 
-          if (debug) Serial.print(num);
-          if (debug) Serial.print(" #");
-          if (debug) Serial.println(i);
-          continue;
+        for (int num = 0; num < NUM_COM; num++) {
+            if (server[num]->hasClient()) {
+                for (byte i = 0; i < MAX_NMEA_CLIENTS; i++) {
+                    // find free/disconnected spot
+                    if (!TCPClient[num][i] || !TCPClient[num][i].connected()) {
+                        if (TCPClient[num][i]) TCPClient[num][i].stop();
+                        TCPClient[num][i] = server[num]->available();
+                        debug.print("New client for COM");
+                        debug.print(num);
+                        debug.print(" #");
+                        debug.println(i);
+                        continue;
+                    }
+                }
+                // no free/disconnected spot so reject
+                WiFiClient TmpserverClient = server[num]->available();
+                TmpserverClient.stop();
+            }
         }
-      }
-      //no free/disconnected spot so reject
-      WiFiClient TmpserverClient = server[num]->available();
-      TmpserverClient.stop();
-    }
-  }
 #endif
- 
-  for(int num= 0; num < NUM_COM ; num++)
-  {
-    if(COM[num] != NULL)          
-    {
-      for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-      {               
-        if(TCPClient[num][cln]) 
-        {
-          while(TCPClient[num][cln].available())
-          {
-            buf1[num][i1[num]] = TCPClient[num][cln].read(); // read char from client
-            if(i1[num]<bufferSize-1) i1[num]++;
-          } 
 
-          COM[num]->write(buf1[num], i1[num]); // now send to UART(num):
-          i1[num] = 0;
-        }
-      }
-  
-      if(COM[num]->available())
-      {
-        while(COM[num]->available())
-        {     
-          buf2[num][i2[num]] = COM[num]->read(); // read char from UART(num)
-          if(i2[num]<bufferSize-1) i2[num]++;
-        }
-        // now send to WiFi:
-        for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-        {   
-          if(TCPClient[num][cln])                     
-            TCPClient[num][cln].write(buf2[num], i2[num]);
-        }
+        for (int num = 0; num < NUM_COM; num++) {
+            if (COM[num] != NULL) {
+#ifdef PROTOCOL_TCP
+                for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++) {
+                    if (TCPClient[num][cln]) {
+                        while (TCPClient[num][cln].available()) {
+                            buf1[num][i1[num]] =
+                                TCPClient[num][cln]
+                                    .read();  // read char from client
+                            i1[num]++;
+                            if (i1[num] == BUFFERSIZE - 1) break;
+                        }
+
+                        COM[num]->write(buf1[num],
+                                        i1[num]);  // now send to UART(num):
+                        i1[num] = 0;
+                    }
+                }
+#endif
+
+                if (COM[num]->available()) {
+                    while (COM[num]->available()) {
+                        buf2[num][i2[num]] =
+                            COM[num]->read();  // read char from UART(num)
+                        i2[num]++;
+                        if (i2[num] == BUFFERSIZE - 1) break;
+                    }
+
+#ifdef PROTOCOL_TCP
+                    for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++) {
+                        if (TCPClient[num][cln])
+                            TCPClient[num][cln].write(buf2[num], i2[num]);
+                    }
+#endif
 
 #ifdef ESP32
-#ifdef BLUETOOTH        
-        // now send to Bluetooth:
-        if(SerialBT.hasClient())      
-          SerialBT.write(buf2[num], i2[num]);               
+#ifdef PROTOCOL_UDP
+                    udp.broadcastTo(buf2[num], i2[num], udp_port[num]);
+#endif
+#ifdef BLUETOOTH
+                    // now send to Bluetooth:
+                    if (num == BLUETOOTH && SerialBT.hasClient())
+                        SerialBT.write(buf2[num], i2[num]);
 #endif
 #endif
-        i2[num] = 0;
-      }
-    }    
-  }
-}
+                    i2[num] = 0;
+                }
+            }
+        }
+    }
--- config.h
+++ config.h
@@ -1,29 +1,45 @@
 /*********************************************************************************
  * config.h
  * 
- * Added compatibility for ESP8266 -- Yuri - Aug 2021
+ * Added compatibility for ESP8266  -- Yuri - Aug 2021
+ * Incremented  version to 2.0      -- Yuri - Apr 2023
+ * Implemented UDP + bug fixes      -- Yuri - Apr 2023
  * 
 *********************************************************************************/
+#ifndef CONFIG_H
+#define CONFIG_H
 
-#define OTA_HANDLER     // uncomment to enable OTA programming
-#define MODE_STA        // or MODE_AP for access point
-#define PROTOCOL_TCP    // TCP only, PROTOCOL_UDP is not presently implemented
-//#define BLUETOOTH     // uncomment to create a bluetooth serial bridge (ESP32 only)
-//#define BATTERY_SAVER // uncomment to reduce wifi power
-
-const char *ssid = "ssid";          // SSID to broadcast (or join)
-const char *pw = "password";        // wifi password
-const char *mdns_name = "esp32";    // hostname for STA mode mDNS
-IPAddress ip(192, 168, 4, 1);       // static IP for AP mode
-IPAddress netmask(255, 255, 255, 0);
-
-const bool debug = true; // set false to suppress system serial messages
+#define DEBUG  // comment to suppress system serial messages
 
 #ifdef ESP32
-#define VERSION "1.11-ESP32"
+#include <WiFi.h>
 #elif defined(ESP8266)
-#define VERSION "1.11-ESP8266"
+#include <ESP8266WiFi.h>
 #endif
+
+#define OTA_HANDLER          // uncomment to enable OTA programming
+
+#define SSID     "ssid"      // SSID to join (or broadcast)
+#define PASSWD   "password"  // wiFi password
+#define HOSTNAME "esp32"     // hostname for STA mode mDNS
+
+#define BUFFERSIZE 1024
+
+#ifdef ESP32
+#define VERSION "2.0-ESP32"
+#elif defined(ESP8266)
+#define VERSION "2.0-ESP8266"
+#endif
+
+#define MODE_STA               // MODE_STA or MODE_AP
+#define PROTOCOL_TCP           // uncomment to enable TCP server
+#define MAX_NMEA_CLIENTS 4     // max TCP clients
+#define PROTOCOL_UDP           // uncomment to enable UDP broadcast (ESP32 only)
+//#define BLUETOOTH 0          // uncomment to create a bluetooth serial bridge on the indicated serial port (ESP32 only)
+//#define BATTERY_SAVER        // uncomment to reduce wifi power
+
+#define STATIC_IP IPAddress(192, 168,   4, 1)  // static IP for MODE_AP
+#define NETMASK   IPAddress(255, 255, 255, 0)  // netmask for MODE_AP
 
 #ifdef ESP32
 #define NUM_COM   3    // 3 available on ESP32
@@ -32,22 +48,36 @@ const bool debug = true; // set false to suppress system serial messages
 #endif
 
 /**************************  COM Port 0 *******************************/
-#define UART_BAUD0 57600          // Baudrate UART0
+#define UART_BAUD0 115200         // Baudrate UART0
 #define SERIAL_PARAM0 SERIAL_8N1  // Data/Parity/Stop UART0 (use SWSERIAL_* for ESP8266)
 #define SERIAL0_TXPIN 1           // transmit Pin UART0
 #define SERIAL0_RXPIN 21          // receive Pin UART0
-#define SERIAL0_TCP_PORT 8880     // Wifi Port UART0
+#define SERIAL0_TCP_PORT 8880     // TCP Port UART0
+#define SERIAL0_UDP_PORT 14550    // UDP Port UART0           (ESP32 only)
 /*************************  COM Port 1 *******************************/
-#define UART_BAUD1 57600            // Baudrate UART1
+#define UART_BAUD1 115200           // Baudrate UART1
 #define SERIAL_PARAM1 SERIAL_8N1    // Data/Parity/Stop UART1 (use SWSERIAL_* for ESP8266)
 #define SERIAL1_TXPIN 17            // transmit Pin UART1
 #define SERIAL1_RXPIN 16            // receive Pin UART1
-#define SERIAL1_TCP_PORT 8881       // Wifi Port UART1
+#define SERIAL1_TCP_PORT 8881       // TCP Port UART1
+#define SERIAL1_UDP_PORT 14551      // UDP Port UART1         (ESP32 only)
 /*************************  COM Port 2 *******************************/
-#define UART_BAUD2 57600            // Baudrate UART2         (ESP32 only)
+#define UART_BAUD2 115200           // Baudrate UART2         (ESP32 only)
 #define SERIAL_PARAM2 SERIAL_8N1    // Data/Parity/Stop UART2 (ESP32 only)
 #define SERIAL2_TXPIN 4             // transmit Pin UART2     (ESP32 only)
 #define SERIAL2_RXPIN 15            // receive Pin UART2      (ESP32 only)
-#define SERIAL2_TCP_PORT 8882       // Wifi Port UART2        (ESP32 only)
+#define SERIAL2_TCP_PORT 8882       // TCP Port UART2         (ESP32 only)
+#define SERIAL2_UDP_PORT 14552      // UDP Port UART2         (ESP32 only)
 
-#define bufferSize 1024
+/**************************  DO NOT EDIT BELOW HERE **************************/
+// perhaps a clever way to include/exclude serial debug messages...
+#ifdef DEBUG
+    #define debug Serial
+#else
+    class Debug: public Print {
+        virtual size_t write(byte b) { return 0; }
+    };
+    Debug debug;
+#endif
+
+#endif  // CONFIG_H
